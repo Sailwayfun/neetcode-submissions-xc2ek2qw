6df54cff5e6e44057384d18ee122a783dace1e53
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-01 10:39:39 +0800

    Add: balanced-binary-tree - submission-6

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-6.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        function getHeight(node) {
+            if(!node) return 0;
+            const leftHeight = getHeight(node.right);
+            const rightHeight = getHeight(node.left);
+            if(leftHeight === -1 || rightHeight === -1) return -1;
+            
+            if(Math.abs(leftHeight - rightHeight) > 1) {
+                return -1;
+            }
+            return 1 + Math.max(leftHeight, rightHeight);
+        }
+
+        return getHeight(root) !== -1;
+    }
+}
