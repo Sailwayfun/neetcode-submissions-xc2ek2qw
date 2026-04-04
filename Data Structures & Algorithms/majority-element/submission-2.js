3aac3832120c7badc8f000ee8d9276e47d2c675f
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-04 12:16:13 +0800

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let res = nums[0];
+        let count = 1;
+
+        for(let i = 1; i < nums.length; i++) {
+            if(nums[i] === res) {
+                count++;
+            } else if(count > 0) {
+                count--;
+            } else {
+                res = nums[i];
+                count++;
+            }
+        }
+
+        return res;
+    }
+}
