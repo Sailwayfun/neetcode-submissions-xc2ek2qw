22199495de783618e9a1cb991d8a08803c7ad4e3
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-04 16:03:40 +0800

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s) {
+        let res = 0;
+        for(let i = 1; i < s.length; i++) {
+            res += Math.abs(s[i].charCodeAt(0) - s[i - 1].charCodeAt(0));
+        }
+
+        return res;
+    }
+}
