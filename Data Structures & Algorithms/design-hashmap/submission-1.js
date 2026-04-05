b3bd2458f6a029cfb1ce62f57de948298485e77d
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-05 15:16:18 +0800

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.js
@@ -0,0 +1,87 @@
+class ListNode {
+    constructor(key = -1, value = -1, next = undefined) {
+        this.key = key;
+        this.value = value;
+        this.next = (next === undefined) ? null : next;
+    }
+}
+
+
+class MyHashMap {
+    constructor() {
+        this.map = [];
+        this.size = 10000;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        const index = key % this.size;
+
+        if(!this.map[index]) {
+            this.map[index] = new ListNode();
+        }
+
+        let curr = this.map[index];
+
+        while(curr.next) {
+            if(curr.next.key === key) {
+                curr.next.value = value;
+                return;
+            }
+            curr = curr.next;
+        }
+        curr.next = new ListNode(key, value);
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        const index = key % this.size;
+
+        let curr = this.map[index];
+
+        if(!curr) return -1;
+
+        curr = curr.next;
+        
+        while(curr) {
+            if(curr.key === key) {
+                return curr.value;
+            }
+            curr = curr.next;
+        }
+        return -1;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        const index = key % this.size;
+        
+        let curr = this.map[index];
+        
+        while(curr && curr.next) {
+            if(curr.next.key === key) {
+                curr.next = curr.next.next;
+                return;
+            }
+            curr = curr.next;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
