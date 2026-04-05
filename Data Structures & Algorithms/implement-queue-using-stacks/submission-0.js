b26b978fe8a85cb0973655505817dbad98f9b1bb
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-05 09:06:03 +0800

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.js
@@ -0,0 +1,62 @@
+class MyQueue {
+    constructor() {
+        this.stack1 = [];
+        this.stack2 = [];
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+        this.stack1.push(x);
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        if(this.stack2.length > 0) {
+            return this.stack2.pop();
+        }
+
+        let size = this.stack1.length;
+        while(size--) {
+            this.stack2.push(this.stack1.pop());
+        }
+
+        return this.stack2.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    peek() {
+        if(this.stack2.length > 0) {
+            return this.stack2[this.stack2.length - 1];
+        }
+
+        let size = this.stack1.length;
+        while(size--) {
+            this.stack2.push(this.stack1.pop());
+        }
+
+        return this.stack2[this.stack2.length - 1];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty() {
+        return this.stack1.length === 0 && this.stack2.length === 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
