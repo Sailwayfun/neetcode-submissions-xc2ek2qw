979332779fea53c84327c445c1b78c6c7b3bbe1c
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-06 09:20:53 +0800

    Add: valid-palindrome-ii - submission-10

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-10.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        function isPalindrome(s, i, j) {
+            while(i < j) {
+                if(s[i] !== s[j]) return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        let left = 0;
+        let right = s.length - 1;
+
+        while(left < right) {
+            if(s[left] !== s[right]) {
+                return isPalindrome(s, left+1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
