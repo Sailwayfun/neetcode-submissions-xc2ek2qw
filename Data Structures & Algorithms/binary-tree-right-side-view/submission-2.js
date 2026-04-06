5e4e9ff27093ce0fefa869c8aa3debf159f4fe75
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-06 10:31:45 +0800

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        const res = [];
+
+        if(!root) return res;
+
+        const queue = [root];
+
+        let head = 0;
+
+        while(head < queue.length) {
+            const levelSize = queue.length - head;
+
+            for(let i = 0; i < levelSize; i++) {
+                const front = queue[head++];
+                if(front.left) queue.push(front.left);
+                if(front.right) queue.push(front.right);
+
+                if(i === levelSize - 1) {
+                    res.push(front.val);
+                }
+            }
+
+        }
+
+        return res;
+    }
+}
