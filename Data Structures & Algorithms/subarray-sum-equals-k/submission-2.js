a65c33d7e9a1b9f977279dbf7661761355d3de79
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-06 16:52:24 +0800

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraySum(nums, k) {
+        const suffixMap = new Map();
+        
+        suffixMap.set(0, 1);
+        let currentSuffix = 0;
+        let count = 0;
+        
+        for(let i = nums.length - 1; i >= 0; i--) {
+            const num = nums[i];
+            currentSuffix += num;
+            
+            //s[i] - s[j + 1] = k
+            const target = currentSuffix - k;
+
+            if(suffixMap.has(target)) {
+                count += suffixMap.get(target);
+            }
+
+            suffixMap.set(currentSuffix, (suffixMap.get(currentSuffix) || 0) + 1);
+
+        }
+
+        return count;
+    }
+}
