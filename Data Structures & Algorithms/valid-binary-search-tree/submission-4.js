7e2ac56d707a5c0f97e064f5b96feac38128077a
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-07 22:14:56 +0800

    Add: valid-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+        let prevVal = -Infinity;
+        let curr = root;
+
+        //Morris Traversal: find precessor before traversal
+
+        while(curr) {
+            if(!curr.left) {
+                if(prevVal >= curr.val) return false;
+                prevVal = curr.val;
+                curr = curr.right;
+            } else {
+                let pre = curr.left;
+                while(pre.right && pre.right !== curr) {
+                    pre = pre.right;
+                }
+
+                if(!pre.right) {
+                    //first round, creat cycle from predecessor
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    //second round, break the cycle
+                    pre.right = null;
+                    
+                    if(prevVal >= curr.val) return false;
+                    prevVal = curr.val;
+                    curr = curr.right;
+                }
+
+            }
+        }
+
+        return true;
+    }
+}
