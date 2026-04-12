aa810f54b62df0309790ab20d4c11ee79bc6d417
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-12 16:13:48 +0800

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums) {
+        let n = nums.length;
+
+        for(let i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+                const targetIdx = nums[i] - 1;
+                [nums[i], nums[targetIdx]] = [nums[targetIdx], nums[i]];
+            }
+        }
+
+        for(let i = 0; i < n; i++) {
+            if(nums[i] !== i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
