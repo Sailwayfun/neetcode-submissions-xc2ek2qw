5ddf598f6694758083b75ff5dde30358ffdc7b5e
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-21 22:13:29 +0800

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {character[]} tasks
+     * @param {number} n
+     * @return {number}
+     */
+    leastInterval(tasks, n) {
+        const freq = new Map();
+
+        let maxFreq = 0;
+
+        for(const t of tasks) {
+            const curr = (freq.get(t) || 0) + 1;
+            freq.set(t, curr);
+
+            maxFreq = Math.max(maxFreq, curr);
+        }
+
+        let maxCount = 0;
+        for(const f of freq.values()) {
+            if(f === maxFreq) {
+                maxCount++;
+            }
+        }
+
+        const idleSlots = (maxFreq - 1) * (n + 1);
+
+        return Math.max(tasks.length, idleSlots + maxCount);
+    }
+}
