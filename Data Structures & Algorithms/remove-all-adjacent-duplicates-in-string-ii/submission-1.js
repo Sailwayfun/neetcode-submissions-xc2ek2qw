b6eeb0fbe146d615df93f6354bf5c5632a8d7d15
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-25 16:39:22 +0800

    Add: remove-all-adjacent-duplicates-in-string-ii - submission-1

---
+++ Data Structures & Algorithms/remove-all-adjacent-duplicates-in-string-ii/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {string}
+     */
+    removeDuplicates(s, k) {
+        const stack = [];
+
+        for(let i = 0; i < s.length; i++) {
+           if(stack.length > 0 && s[i] === stack[stack.length - 1].val) {
+                stack[stack.length - 1].occurrence++;
+           } else {
+                stack.push({val: s[i], occurrence: 1});
+           }
+
+           if(stack[stack.length - 1].occurrence === k) {
+                stack.pop();
+           }
+        }
+
+        let res = "";
+
+        for(let {val, occurrence} of stack) {
+           res += val.repeat(occurrence);
+        }
+
+        return res;
+    }
+}
