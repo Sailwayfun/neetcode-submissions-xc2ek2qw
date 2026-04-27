2cc241e665de1a918e710fee87d9cd1fbe23a576
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-27 18:55:01 +0800

    Add: grumpy-bookstore-owner - submission-0

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} customers
+     * @param {number[]} grumpy
+     * @param {number} minutes
+     * @return {number}
+     */
+    maxSatisfied(customers, grumpy, minutes) {
+        let maxSave = -Infinity;
+
+        let savedCount = 0;
+        let satisfiedCount = 0;
+
+        let left = 0;
+        for(let right = 0; right < customers.length; right++) {
+            const customer = customers[right];
+            if(grumpy[right] === 0) {
+                satisfiedCount += customers[right];
+            } else {
+                savedCount += customers[right];
+            }
+            
+            if(right - left === minutes) {
+                if(grumpy[left] === 1) {
+                    savedCount -= customers[left];
+                }
+                left++;
+            }
+
+            maxSave = Math.max(maxSave, savedCount);
+        }
+
+        return satisfiedCount + maxSave;
+    }
+}
