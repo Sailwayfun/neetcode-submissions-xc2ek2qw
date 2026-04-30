033637705cb2d06952ff10a4e3ef5c7ea58056eb
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-30 09:00:32 +0800

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number} k
+     * @return {number[][]}
+     */
+    combine(n, k) {
+        const res = [];
+
+        function backtrack(num, combination) {
+            if(combination.length === k) {
+                res.push([...combination]);
+            }
+
+            for(let i = num; i <= n; i++) {
+                combination.push(i);
+                backtrack(i + 1, combination);
+                combination.pop();
+            }
+        }
+
+        backtrack(1, []);
+
+        return res;
+    }
+}
