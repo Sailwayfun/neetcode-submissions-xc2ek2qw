1bc105c98fe55d1a81482c46d32e00b3220539b3
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-04-30 09:02:55 +0800

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number} k
+     * @return {number[][]}
+     */
+    combine(n, k) {
+        const res = [];
+
+        function backtrack(num, combination) {
+            if(combination.length === k) {
+                res.push([...combination]);
+                return;
+            }
+
+            for(let i = num; i <= n; i++) {
+                if(n - num + 1 < k - combination.length) return;
+                combination.push(i);
+                backtrack(i + 1, combination);
+                combination.pop();
+            }
+        }
+
+        backtrack(1, []);
+
+        return res;
+    }
+}
