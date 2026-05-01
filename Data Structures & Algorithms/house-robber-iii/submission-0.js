dc12da946274b53809f55733d91b237b24a20ea7
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-01 09:13:46 +0800

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    rob(root) {
+        function dfs(node) {
+            if(!node) {
+                return [0, 0];
+            }
+            const [leftSkip, leftRob] = dfs(node.left);
+            const [rightSkip, rightRob] = dfs(node.right);
+
+            return [Math.max(leftRob, leftSkip) + Math.max(rightRob, rightSkip), node.val + leftSkip + rightSkip];
+        }
+
+        return Math.max(dfs(root)[0], dfs(root)[1]);
+    }
+}
