59d69a6227f4e339308f7f2ac49c6b1e7f31dfc9
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-02 17:09:02 +0800

    Add: combination-sum-iv - submission-2

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-2.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    combinationSum4(nums, target) {
+        const cache = new Array(target + 1);
+        
+        function combinationSum(sum) {
+            if(sum === target) return 1;
+            if(sum > target) return 0;
+
+            if(cache[sum] !== undefined) return cache[sum];
+
+            let res = 0;
+            for(const num of nums) {
+                res += combinationSum(sum + num);
+            }
+
+            cache[sum] = res;
+
+            return res;
+        }
+
+        return combinationSum(0);
+    }
+    //
+    //0 -> 1 -> 2 or 4 or 3. -> 3 or 4 or 4
+    // 0 -> 3 -> 4 or 6 or 5
+    // 0 -> 2 --> 3 or 5 or 4
+}
