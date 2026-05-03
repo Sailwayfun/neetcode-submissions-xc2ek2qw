2d070318181667d5508a2f5506b3500561c4257b
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-03 08:27:32 +0800

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        const cache = new Array(nums.length);
+        
+        function dfs(i) {
+            if(i >= nums.length) {
+                return 0;
+            }
+
+            if(cache[i]) return cache[i];
+
+            cache[i] = Math.max(nums[i] + dfs(i + 2), dfs(i + 1));
+
+            return cache[i];
+        }
+
+        return dfs(0);
+    }
+}
