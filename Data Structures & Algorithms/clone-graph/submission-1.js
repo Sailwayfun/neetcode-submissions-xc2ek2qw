820bc1335c2774c8bf41e28d7b9166a2f80dbd38
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-09 20:21:01 +0800

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        const nodeMap = new Map();
+        
+        function dfs(node) {
+            if(!node) return null;
+            
+            if(nodeMap.has(node)) return nodeMap.get(node);
+
+            const clonedNode = new Node(node.val, []);
+            
+            nodeMap.set(node, clonedNode);
+            
+            for(const neighbor of node.neighbors) {
+                const clonedNeighbor = dfs(neighbor);
+                clonedNode.neighbors.push(clonedNeighbor);
+            }
+
+            return nodeMap.get(node);
+        }
+
+        return dfs(node);
+    }
+}
