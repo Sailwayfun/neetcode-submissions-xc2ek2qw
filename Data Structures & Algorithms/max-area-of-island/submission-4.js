c0793153f58158667e2f48f9c374dad878c01a6e
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-09 10:07:02 +0800

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.js
@@ -0,0 +1,64 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+
+        const uf = new UnionFind(rows, cols);
+        
+        for(let i = 0; i < rows; i++) {
+            for(let j = 0; j < cols; j++) {
+                if(grid[i][j] === 1) {
+                    uf.sizes[i * cols + j] = 1;
+                }
+            }
+        }
+
+        for(let i = 0; i < rows; i++) {
+            for(let j = 0; j < cols; j++) {
+                const currIndex = i * cols + j;
+                if(grid[i][j] === 1) {
+                    if(i + 1 < rows && grid[i+1][j] === 1) {
+                        const rightIndex = (i + 1) * cols + j;
+                        uf.union(currIndex, rightIndex);
+                    }
+                    if(j + 1 < cols && grid[i][j + 1] === 1) {
+                        const bottomIndex = i * cols + j + 1;
+                        uf.union(currIndex, bottomIndex);
+                    }
+                }
+            }
+        }
+
+        return Math.max(...uf.sizes);
+    }
+}
+
+class UnionFind {
+    constructor(rows, cols) {
+        this.parent = Array.from({length: rows * cols}, (_, i) => i);
+        this.sizes = Array.from({length: rows * cols}, () => 0);
+    }
+
+    find(i) {
+        if(this.parent[i] === i) return i;
+        return this.parent[i] = this.find(this.parent[i]);
+    }
+
+    union(i, j) {
+        const rootI = this.find(i);
+        const rootJ = this.find(j);
+        if(rootI !== rootJ) {
+            if(this.sizes[rootI] <= this.sizes[rootJ]) {
+                this.parent[rootI] = rootJ;
+                this.sizes[rootJ] += this.sizes[rootI];
+            } else {
+                this.parent[rootJ] = rootI;
+                this.sizes[rootI] += this.sizes[rootJ];
+            }
+        }
+    }
+}
