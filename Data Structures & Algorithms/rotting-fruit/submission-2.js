6dc7a76b3b1c368ff8a429b38403e55cc4b2c8a4
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-10 09:01:30 +0800

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.js
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    orangesRotting(grid) {
+        if(!grid || !grid[0]) return -1;
+        
+        const [rows, cols] = [grid.length, grid[0].length];
+
+        const queue = [];
+        
+        let freshCount = 0;
+
+        for(let i = 0; i < rows; i++) {
+            for(let j = 0; j < cols; j++) {
+                if(grid[i][j] === 2) {
+                    queue.push([i, j]);
+                }
+                if(grid[i][j] === 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        let time = -1;
+
+        if(freshCount === 0) return 0;
+
+        while(queue.length > 0) {
+            const size = queue.length;
+
+            for(let i = 0; i < size; i++) {
+                const first = queue.shift();
+                const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+                for(const [r, c] of directions) {
+                    const nextR = first[0] + r;
+                    const nextC = first[1] + c;
+
+                    if(nextR < 0 || nextC < 0 || nextR >= rows || nextC >= cols) continue;
+
+                    if(grid[nextR][nextC] === 1) {
+                        grid[nextR][nextC] = 2;
+                        queue.push([nextR, nextC]);
+                    }
+                }
+            }
+
+            time++;
+        }
+
+        for(let i = 0; i < rows; i++) {
+            for(let j = 0; j < cols; j++) {
+                if(grid[i][j] === 1) return -1;
+            }
+        }
+
+        return time;
+    }
+}
