308ce810fcf54195e0e55c55f7d66d751e67da3c
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-11 22:11:18 +0800

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    numDecodings(s) {
+        function dfs(index, cache) {
+            if(index === s.length) return 1;
+
+            if(s[index] === "0") return 0;
+
+            if(cache[index]) return cache[index];
+
+            let res = dfs(index + 1, cache);
+
+            if(index + 1 < s.length) {
+                const twoDigits = Number(s[index]) * 10 + Number(s[index + 1]);
+                if(twoDigits <= 26) {
+                    res += dfs(index + 2, cache);
+                }
+            }
+
+            return cache[index] = res;
+
+
+        }
+
+        return dfs(0, []);
+    }
+}
