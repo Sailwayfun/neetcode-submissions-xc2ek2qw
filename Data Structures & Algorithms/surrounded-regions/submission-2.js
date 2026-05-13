83cf1290b9de4c8970787030645eb43ce8134064
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-13 08:48:12 +0800

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {void} Do not return anything, modify board in-place instead.
+     */
+    solve(board) {
+        const [rows, cols] = [board.length, board[0].length];
+        
+        function dfs(i, j) {
+            if(i < 0 || j < 0 || i >= rows || j >= cols || board[i][j] !== "O") {
+                return;
+            }
+
+            board[i][j] = "#";
+
+            dfs(i - 1, j);
+            dfs(i + 1, j);
+            dfs(i, j - 1);
+            dfs(i, j + 1);
+        }
+
+        for(let i = 0; i < rows; i++) {
+            dfs(i, 0);
+            dfs(i, cols - 1);
+        }
+
+        for(let j = 0; j < cols; j++) {
+            dfs(0, j);
+            dfs(rows - 1, j);
+        }
+
+        for(let i = 0; i < rows; i++) {
+            for(let j = 0; j < cols; j++) {
+                if(board[i][j] === "#") {
+                    board[i][j] = "O";
+                } else if(board[i][j] === "O") {
+                    board[i][j] = "X";
+                }
+            }
+        }
+    }
+}
