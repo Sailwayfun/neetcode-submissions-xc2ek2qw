f7c21c233e94ec293c59d4cc6a51519730d0afd7
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-19 07:51:23 +0800

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        const n = s.length;
+        const dp = new Array(n + 1).fill(false);
+        dp[0] = true;
+
+        for(let i = 1; i <= n; i++) {
+            for(let j = 0; j < wordDict.length; j++) {
+                const len = wordDict[j].length;
+                if(i < len) continue;
+
+                if(s.slice(i - len, i) === wordDict[j] && dp[i - len] === true) {
+                    dp[i] = true;
+                }
+            }
+        }
+
+        return dp.pop() === true;
+    }
+}
