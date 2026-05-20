1f3e15dc145a80bb6df764db596fcaa4d0d452b0
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-20 20:53:01 +0800

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if(edges.length !== n - 1) return false;
+
+        const adjList = Array.from({length: n}, () => []);
+
+        for(const [a, b] of edges) {
+            adjList[a].push(b);
+            adjList[b].push(a);
+        }
+
+        const visited = new Set();
+
+        function dfs(node, parent) {
+            if(visited.has(node)) return false;
+
+            visited.add(node);
+
+            for(const n of adjList[node]) {
+                if(n === parent) continue;
+                if(dfs(n, node) === false) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return dfs(0, -1) && visited.size === n;
+    }
+}
