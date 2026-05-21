4c2d3dc2070556e6e5c56e5fc927f0ed76fdfb8c
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-21 20:14:50 +0800

    Add: partition-equal-subset-sum - submission-5

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-5.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        const n = nums.length;
+        const sum = nums.reduce((a, b) => a + b);
+
+        if(sum % 2 !== 0) return false;
+
+        const target = sum / 2;
+
+        let dp = new Set();
+        dp.add(0);
+
+        for(let i = n - 1; i >= 0; i--) {
+            const nextDP = new Set();
+            
+            for(const t of dp) {
+                nextDP.add(t + nums[i]);
+                nextDP.add(t);    
+            }
+
+            dp = nextDP;
+
+        }
+
+        return dp.has(target);
+    }
+}
