b4fcef9f2fa054057a5e48d8774a6a35e72b7938
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-21 08:37:21 +0800

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        const sum = nums.reduce((a, b) => a + b);
+
+        if(sum % 2 !== 0) return false;
+
+        const target = sum / 2;
+
+        const cache = Array.from({length: nums.length}, () => new Array(sum + 1).fill(null));
+
+        function dfs(index, subSum) {
+            if(index >= nums.length) return false;
+            
+            if(subSum === target) {
+                return cache[index][subSum] = true;
+            }
+
+            if(cache[index][subSum] !== null) return cache[index][subSum];
+            
+            const nextIdx = index + 1;
+            const inclusiveSum = subSum + nums[index];
+
+            if(inclusiveSum <= target) {
+                if(dfs(nextIdx, inclusiveSum)) return cache[index][subSum] = true;
+            }
+            
+            if(dfs(nextIdx, subSum)) return cache[index][subSum] = true;
+
+            return cache[index][subSum] = false;
+        }
+
+        return dfs(0, 0);
+    }
+}
