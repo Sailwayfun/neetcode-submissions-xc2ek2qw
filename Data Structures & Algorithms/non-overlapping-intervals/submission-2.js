e1557584bb19650b415567209557882e415354f6
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-24 07:19:59 +0800

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        const n = intervals.length;
+
+        if(n <= 1) return 0;
+
+        intervals.sort((a, b) => a[1] - b[1]);
+
+        let prevEnd = intervals[0][1];
+
+        let removeCount = 0;
+
+        for(let i = 1; i < n; i++) {
+            const curr = intervals[i];
+
+            if(curr[0] >= prevEnd) {
+                prevEnd = curr[1];
+            } else {
+                removeCount++;
+            }
+        }
+
+        return removeCount;
+    }
+}
