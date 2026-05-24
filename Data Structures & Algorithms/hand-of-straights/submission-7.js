83658aaa672fd0a01e65618f26a35fbb560788bb
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-24 11:28:15 +0800

    Add: hand-of-straights - submission-7

---
+++ Data Structures & Algorithms/hand-of-straights/submission-7.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} hand
+     * @param {number} groupSize
+     * @return {boolean}
+     */
+    isNStraightHand(hand, groupSize) {
+        if(hand.length % groupSize !== 0) return false;
+
+        const freq = new Map();
+
+        for(const h of hand) {
+            freq.set(h, (freq.get(h) || 0) + 1);
+        }
+
+        hand.sort((a, b) => a - b);
+
+        for(const h of hand) {
+           if(!freq.has(h) || freq.get(h) <= 0) continue;
+
+           freq.set(h, freq.get(h) - 1);
+
+           for(let j = 1; j < groupSize; j++) {
+                if(!freq.has(h + j) || freq.get(h + j) === 0) return false;
+                freq.set(h + j, freq.get(h + j) - 1);
+           }
+        }
+
+        return true;
+    }
+}
