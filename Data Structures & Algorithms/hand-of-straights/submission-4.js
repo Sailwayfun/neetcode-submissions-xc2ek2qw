96b023006c95941cd10ff126ee4c71d24b3d5b49
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-24 10:48:15 +0800

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} hand
+     * @param {number} groupSize
+     * @return {boolean}
+     */
+    isNStraightHand(hand, groupSize) {
+        if(hand.length % groupSize !== 0) return false;
+
+        const freq = new Map();
+
+        for(const h of hand) {
+            freq.set(h, (freq.get(h) || 0) + 1);
+        }
+
+        hand.sort((a, b) => a - b);
+
+        for(const h of hand) {
+            if(freq.get(h - 1) > 0 || freq.get(h) <= 0) continue;
+            
+            for(let j = 0; j < groupSize; j++) {
+                if(freq.get(h + j) <= 0 || !freq.has(h + j)) return false;
+                freq.set(h + j, freq.get(h + j) - 1);
+            }
+        }
+
+        return true;
+    }
+}
