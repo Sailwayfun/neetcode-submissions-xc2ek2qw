61990126166a932dadf613895d71dd96acf20000
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-24 09:16:12 +0800

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.js
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        const n = intervals.length;
+
+        if(n <= 1) return true;
+
+        intervals.sort((a, b) => a.start - b.start);
+
+        let prevEnd = intervals[0].end;
+
+        for(let i = 1; i < n; i++) {
+            const curr = intervals[i];
+            if(curr.start < prevEnd) return false;
+            prevEnd = Math.max(prevEnd, curr.end);
+        }
+
+        return true;
+    }
+}
