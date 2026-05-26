c626bdff272beb081f7bb3e9daf33adc3d63cd0b
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-26 10:12:10 +0800

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        const cache = Array.from({length: m}, () => new Array(n).fill(-1));
+        
+        function dfs(i, j) {
+            if(i >= m || j >= n) {
+                return 0;
+            }
+
+            if(cache[i][j] !== -1) return cache[i][j];
+
+            if(i === m - 1 && j === n - 1) {
+                return 1;
+            }
+            
+            return cache[i][j] = dfs(i + 1, j) + dfs(i, j + 1);
+        }
+
+        return dfs(0, 0);
+    }
+}
