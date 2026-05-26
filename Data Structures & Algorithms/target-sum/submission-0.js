b26cd64212ed38f2ab2afd1217db19d0268b383a
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-26 19:06:25 +0800

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    findTargetSumWays(nums, target) {
+        const n = nums.length;
+        const m = nums.reduce((a, b) => a + b);
+
+        const cache = Array.from({length: n}, () => new Array(2 * m + 1).fill(-1));
+
+        function dfs(index, currSum) {
+            if(index === n) {
+                if(currSum === target) return 1;
+                return 0;
+            }
+
+            if(cache[index][currSum + m] !== -1) return cache[index][currSum + m];
+
+            return cache[index][currSum + m] = dfs(index + 1, currSum - nums[index]) + dfs(index + 1, currSum + nums[index]);
+        }
+
+        return dfs(0, 0);
+    }
+}
