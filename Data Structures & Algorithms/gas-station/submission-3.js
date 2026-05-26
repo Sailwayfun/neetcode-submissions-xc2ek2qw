99475b9569c56500727f031a7f1be76c6e57531b
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-26 08:06:00 +0800

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        const totalGas = gas.reduce((a, b) => a + b);
+        const totalCost = cost.reduce((a, b) => a + b);
+
+        if(totalGas < totalCost) return -1;
+
+        let start = 0;
+        let totalTank = 0;
+        let currTank = 0;
+
+        for(let i = 0; i < gas.length; i++) {
+            totalTank += (gas[i] - cost[i]);
+            currTank += (gas[i] - cost[i]);
+
+            if(currTank < 0) {
+                currTank = 0;
+                start = i + 1;
+            }
+        }
+
+        if(totalTank >= 0) return start;
+        return -1;
+    }
+}
