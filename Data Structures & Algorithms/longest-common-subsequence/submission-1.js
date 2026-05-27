c334e44aef5e81b26ec3ddce8c95c28d6b4407ac
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-27 11:45:11 +0800

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} text1
+     * @param {string} text2
+     * @return {number}
+     */
+    longestCommonSubsequence(text1, text2) {
+        const n1 = text1.length;
+        const n2 = text2.length;
+
+        const dp = Array.from({length: n1 + 1}, () => new Array(n2 + 1).fill(0));
+
+        for(let i = 1; i <= n1; i++) {
+            for(let j = 1; j <= n2; j++) {
+                if(text1[i - 1] === text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
+    }
+}
