8e4911380d68c5f4b4296bac0254241b36954993
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-05-28 20:55:06 +0800

    Add: redundant-connection - submission-3

---
+++ Data Structures & Algorithms/redundant-connection/submission-3.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[][]} edges
+     * @return {number[]}
+     */
+    findRedundantConnection(edges) {
+        const n = edges.length;
+
+        const parent = new Array(n + 1);
+        for(let i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        const rank = new Array(n + 1).fill(0);
+
+        function find(i) {
+            if(parent[i] !== i) {
+                parent[i] = find(parent[i]);
+            }
+            return parent[i];
+        }
+
+        for(const e of edges) {
+            const [i, j] = e;
+            const rootI = find(i);
+            const rootJ = find(j);
+
+            if(rootI === rootJ) return e;
+
+            if(rank[rootI] > rank[rootJ]) {
+                parent[rootJ] = rootI;
+            } else if(rank[rootJ] > rank[rootI]) {
+                parent[rootI] = rootJ;
+            } else {
+                parent[rootJ] = rootI;
+                rank[rootI]++;
+            }
+        }
+    }
+}
