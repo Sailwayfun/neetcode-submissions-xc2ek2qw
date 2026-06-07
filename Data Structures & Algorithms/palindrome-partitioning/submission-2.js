2b7a97f66828a24a7c909721ebeabea0a72e524b
sail <124685991+Sailwayfun@users.noreply.github.com>
2026-06-07 10:59:22 +0800

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.js
@@ -0,0 +1,48 @@
+
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string[][]}
+     */
+    partition(s) {
+        const res = [];
+
+        const n = s.length;
+
+        function isPalindrome(start, end) {
+            if(start === end) return true;
+
+            let i = start;
+            let j = end;
+
+            while(i < j) {
+                if(s[i] !== s[j]) return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        const path = [];
+
+        function backtrack(startIdx) {
+            if(startIdx === n) {
+                res.push([...path]);
+                return;
+            }
+
+            for(let i = startIdx; i < n; i++) {
+                if(isPalindrome(startIdx, i)) {
+                    path.push(s.slice(startIdx, i + 1));
+                    backtrack(i + 1);
+                    path.pop();
+                }
+            }
+        }
+        
+
+        backtrack(0);
+
+        return res;
+    }
+}
